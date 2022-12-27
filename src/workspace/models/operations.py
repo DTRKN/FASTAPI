c3154e624221f3379889275e6b3a73349dccde70
DTRKN <taratorkin.den@mail.ru>
2022-12-27 19:46:06 +0500

    DB ok, add functions update, delete, create

--- src/workspace/models/operations.py
+++ src/workspace/models/operations.py
@@ -1,18 +1,26 @@
 from pydantic import BaseModel
 from enum import Enum
-# from datetime import date
+from datetime import date
 from typing import Optional
 from decimal import Decimal
 
 class OperationKind(str, Enum):
     INCOME = 'income'
     OUTCOME = 'outcome'
-class Operation(BaseModel):
-    id: int
-    date: str
+
+class OperationBase(BaseModel):
+    date: date
     kind: OperationKind
     amount: Decimal
     description: Optional[str]
+class Operation(OperationBase):
+    id: int
 
     class Config:
         orm_mode = True
+
+class OperationCreate(OperationBase):
+    pass
+
+class OperationUpdate(OperationBase):
+    pass
--- src/workspace/tables.py
+++ src/workspace/tables.py
@@ -8,7 +8,7 @@ class Operation(Base):
     __tablename__ = 'operations'
 
     id = sa.Column(sa.Integer, primary_key=True)
-    date = sa.Column(sa.String)
+    date = sa.Column(sa.Date)
     kind = sa.Column(sa.String)
     amount = sa.Column(sa.Numeric(10, 2))
     description = sa.Column(sa.String, nullable=True)
